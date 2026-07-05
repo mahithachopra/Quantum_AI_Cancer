a5dcc947e99997074908e7b9b9ab73597d34ae73
mahithachopra <104752575+mahithachopra@users.noreply.github.com>
2026-07-05 12:13:43 +0530

    Add files via upload

---
+++ frontend/src/App.jsx
@@ -0,0 +1,10 @@
+import DashboardLayout from "./layouts/DashboardLayout";
+import Home from "./pages/Home";
+
+export default function App() {
+  return (
+    <DashboardLayout>
+      <Home />
+    </DashboardLayout>
+  );
+}
---
+++ frontend/src/main.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+import { ThemeProvider } from "@mui/material/styles";
+import CssBaseline from "@mui/material/CssBaseline";
+
+import theme from "./theme/theme";
+import AppRoutes from "./routes/AppRoutes";
+import { PipelineProvider } from "./context/PipelineContext";
+
+ReactDOM.createRoot(
+  document.getElementById("root")
+).render(
+  <React.StrictMode>
+    <PipelineProvider>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <AppRoutes />
+      </ThemeProvider>
+    </PipelineProvider>
+  </React.StrictMode>
+);
